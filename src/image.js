6d470957ccac79d90d9a104552597ea18e16c787
Aditya Prasad <aditya.k.prasad@oracle.com>
2021-11-25 20:53:35 -0600

    Assemblyscript version

--- src/image.js
+++ src/image.js
@@ -1,58 +1,86 @@
-var canvas = document.getElementById('canvas');
-var ctx = canvas.getContext('2d');
+
+// Set up the canvas with a 2D rendering context
+var cnv = document.getElementById("canvas");
+var ctx = cnv.getContext("2d");
+
+var memory;
 var img = new Image();
 
 img.onload = function() {
+  // Compute the size of and instantiate the module's memory
+  // 4 bytes for RGBA, 12 bytes for HSV
+  var byteSize = img.width * img.height * 16;
+  var pages = ((byteSize + 0xffff) & ~0xffff) >>> 16;
+  memory = new WebAssembly.Memory({ initial: pages });
+  console.log("Allocated pages:", pages);
+  console.log("Width and height", img.width, img.height);
+  canvas.width = img.width;
+  canvas.height = img.height;
   ctx.drawImage(img, 0, 0, img.width,    img.height,     // source rectangle
                      0, 0, canvas.width, canvas.height); // destination rectangle
 
-  imgData = ctx.getImageData(0, 0, img.width, img.height);
-  var hsvs = getHSVs(imgData.data);
+  var imgData = ctx.getImageData(0, 0, img.width, img.height);  
+  var shift = 0;
 
-  cycle(0, ctx, imgData, hsvs);
+  // Fetch and instantiate the module
+  fetch("../build/optimized.wasm")
+  .then(response => response.arrayBuffer())
+  .then(buffer => WebAssembly.instantiate(buffer, {
+    env: {
+      memory: memory,
+      abort: function() {},
+      trace: console.log,  
+    },
+    config: {}, // TODO
+    Math
+  }))
+  .then(module => {
+    var exports = module.instance.exports;
+    exports.init(img.width, img.height);
+
+    var hsvOffset = img.width*img.height*4;
+    var hsvMem = new Float32Array(memory.buffer, hsvOffset);
+    setHSVs(imgData.data, hsvMem);    
+
+    var mem = new Uint8Array(memory.buffer);
+    var imageData = ctx.createImageData(img.width, img.height);
+    var argb = new Uint8Array(imageData.data.buffer);
+
+    (function update() {
+      setTimeout(update, 0);
+      exports.cycle(shift);
+      shift = (shift + 0.035) % 1;
+
+      argb.set(mem.subarray(0, img.width * img.height * 4)); // copy memory image to image buffer
+      ctx.putImageData(imageData, 0, 0);         // apply image buffer
+    })();
+
+    // Keep rendering the output
+    // var imageData = ctx.createImageData(img.width, img.height);
+    // var argb = new Uint8Array(imageData.data.buffer);
+    // (function render() {
+    //   requestAnimationFrame(render);
+    //   argb.set(mem.subarray(0, img.width * img.height * 4)); // copy memory image to image buffer
+    //   ctx.putImageData(imageData, 0, 0);         // apply image buffer
+    // })();
+  }).catch(err => {
+    alert("Failed to load WASM: " + err.message + " (ad blocker, maybe?)");
+    console.log(err.stack);
+  });
 };
 
-function cycle(shift, ctx, imgData, hsvs) {
-  var clone = imgData.data.slice();
-  colorShift(shift, clone, hsvs);
-
-  imgData.data.set(clone);
-  ctx.putImageData(imgData, 0, 0);
-
-  setTimeout(function() {
-    cycle(shift + .035, ctx, imgData, hsvs);
-  }, 0);
-}
-
-function colorShift(shift, data, hsvs) {
-  console.log(hsvs.length);
-  for (var i = 0; i < hsvs.length; i++) {
-    var [h, s, v] = hsvs[i];
-
-    h = (h + shift) % 1;
-    [r, g, b] = hsv2rgb(h, s, v);    
-
-    data[4*i+0] = r;
-    data[4*i+1] = g;
-    data[4*i+2] = b;
-  }
-}
-
-function getHSVs(data) {
+function setHSVs(data, hsvMem) {
   var hsvs = new Array();
-  for (var i = 0; i < data.length; i+= 4) {
-    [r, g, b] = [data[i], data[i+1], data[i+2]];
-    hsvs.push(rgb2hsv(r, g, b));
+  hsvMem[0] = 123.456;
+  for (var i = 0; i < data.length/4; i+= 1) {
+    [r, g, b] = [data[4*i], data[4*i+1], data[4*i+2]];
+    [h, s, v] = rgb2hsv(r, g, b);  
+    hsvMem[3*i+0] = h;
+    hsvMem[3*i+1] = s;
+    hsvMem[3*i+2] = v;
+    //console.log(i, r, g, b, h, s, v, hsvMem[3*i]);
   }
-  return hsvs;
-}
-
-function rgbToInt(r, g, b) {
-    return (r<<16) + (g<<8) + b;
-}
-
-function intToRgb(int) {
-  return [(int & 0xFF0000)>>16, (int & 0x00FF00)>>8, (int & 0x0000FF)]
+  console.log("hsvMem", hsvMem);
 }
 
 function rgb2hsv(r, g, b) {
@@ -79,29 +107,9 @@ function rgb2hsv(r, g, b) {
   return [ h, s, v ];
 }
 
-function hsv2rgb(h, s, v) {
-  var r, g, b;
-
-  var i = Math.floor(h * 6);
-  var f = h * 6 - i;
-  var p = v * (1 - s);
-  var q = v * (1 - f * s);
-  var t = v * (1 - (1 - f) * s);
-
-  switch (i % 6) {
-    case 0: r = v, g = t, b = p; break;
-    case 1: r = q, g = v, b = p; break;
-    case 2: r = p, g = v, b = t; break;
-    case 3: r = p, g = q, b = v; break;
-    case 4: r = t, g = p, b = v; break;
-    case 5: r = v, g = p, b = q; break;
-  }
-
-  return [ r * 255, g * 255, b * 255 ];
-}
-
-img.src = 'psychonaut.jpg';
-//img.src = 'sky-diamonds.jpg';
-//img.src = 'forest-glade-metta.jpg';
-//img.src = 'palette.jpg';
-//img.src = 'rgby_tiny.jpg';
+// img.src = 'psychonaut.jpg';
+// img.src = 'sky-diamonds.jpg';
+// img.src = 'forest-glade-metta.jpg';
+img.src = 'ayahuasca.jpg';
+// img.src = 'palette.jpg';
+// img.src = 'rgby_tiny.jpg';
